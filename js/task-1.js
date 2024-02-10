9b11145fcc063d39fb9f71292e8e4ed3ccdf3d47
Aleksander <aleksander.0705@gmail.com>
2024-02-10 18:26:23 +0200

    correct task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,19 @@
 function slugify(title) {
-  const array = title.split(" ");
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    array[i] = (element).toLowerCase();      
-  }
-  const slug = array.join("-");
+  const lowerCaseTitle = title.toLowerCase().split(' ');
+  const slug = lowerCaseTitle.join('-');
   return slug;
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+///////////////////////////////////////////////////////////
+// function slugify(title) {
+//   const array = title.split(' ');
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     array[i] = element.toLowerCase();
+//   }
+//   const slug = array.join('-');
+//   return slug;
+// }
